dcbca6a35c827466954070e004f612473bc159bd
Thomas Beresford <tomberesford.06@gmail.com>
2024-08-21 19:18:07 +0100

    Updated Header.css and App.css for improved styling

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// src/components/Header.js
+
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>VET.CT Case Management</h1>
+    </header>
+  );
+}
+
+export default Header;
