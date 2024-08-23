d1c47e65d0740bce938f3ed29d3c06200660c2dd
Thomas Beresford <tomberesford.06@gmail.com>
2024-08-23 12:16:04 +0100

    added footer with details

--- src/components/CaseDetail.js
+++ src/components/CaseDetail.js
@@ -19,8 +19,8 @@ function CaseDetail() {
   }
 
   return (
-    <>
-      <div className="case-detail-container">
+    <div className="case-detail-container">
+      <div className="case-detail-content">
         <div className="case-info">
           <h1>{caseDetail.case_key}</h1>
           <p><strong>Patient Name:</strong> {caseDetail.patient}</p>
@@ -38,15 +38,20 @@ function CaseDetail() {
           <img src={caseDetail.image_url} alt="Case" />
         </div>
       </div>
-      <div className="additional-info">
-        Reported by VET.CT<br />
-        t. +44 01223 790439<br />
-        www.vet-ct.com<br />
-        e. info@vet-ct.com<br />
-        Registered Office VET.CT, Broers Building, 21 JJ Thomson Avenue, Cambridge, CB3 0FA, United Kingdom<br />
-        This report is based on the available history and radiographic interpretation only and not on a physical examination of the patient. It has been prepared specifically for interpretation by the currently licensed and registered veterinary surgeon responsible for the care of this patient.
-      </div>
-    </>
+      <footer className="case-detail-footer">
+        <img src="/assets/vetct-favicon.png" alt="VET.CT Logo" className="footer-logo" />
+        <div className="footer-heading">Reported by VET.CT</div>
+        <p className="footer-text">
+          t. +44 01223 790439<br />
+          www.vet-ct.com<br />
+          e. info@vet-ct.com<br />
+          <br />
+          Registered Office VET.CT, Broers Building, 21 JJ Thomson Avenue, Cambridge, CB3 0FA, United Kingdom<br />
+          <br />
+          This report is based on the available history and radiographic interpretation only and not on a physical examination of the patient. It has been prepared specifically for interpretation by the currently licensed and registered veterinary surgeon responsible for the care of this patient.
+        </p>
+      </footer>
+    </div>
   );
 }
 
